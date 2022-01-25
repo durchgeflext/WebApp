32d7cdece4dd6336c87be319cfa6f3eb9638f953
Felix <felix.wedler@t-online.de>
2022-01-25 18:06:34 +0100

    This is the worst shit I have ever witnessed

--- accel.js
+++ accel.js
@@ -5,12 +5,12 @@ var az = 0;
 
 
 window.ondevicemotion = function(event) { 
-	az = event.acceleration.z
+	//az = event.acceleration.z
 	//document.querySelector("#div_stroke_results").innerHTML = "Z = " + az;
 }
 
 
 function getAccel() {
-    x.innerHTML = "bitch";
+    x.innerHTML = "whore";
 }
 
