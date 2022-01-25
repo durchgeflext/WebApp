201bec6f78e6eb0d3998d2e05862ca980e40cf93
Felix <felix.wedler@t-online.de>
2022-01-25 22:46:22 +0100

    Code cleanup

--- accel.js
+++ accel.js
@@ -1,15 +1,12 @@
 var x = document.getElementById("div_stroke_results");
 var active = false;
 
-let acl = null;
-
 var oldAz = 0;
 var az = 0;
 var counter = 0;
 
 window.ondevicemotion = function(event) {
-    az = event.acceleration.z;
-    az = Math.round(az);
+    az = Math.round(event.acceleration.z);
     if(active) {
         countShakes(az);
     }
@@ -19,25 +16,6 @@ window.ondevicemotion = function(event) {
 function startCounter() {
     startCount();
     setTimeout(stopCount, 10000);
-
-
-    /*startBool = true;
-    setTimeout(2000);
-    startBool = false;
-    x.innerHTML = "worked";*/
-
-    /*setTimeout(10000);
-    startBool = false;*/
-    /*navigator.permissions.query({name: 'accelerometer'}).then(result => {
-        if(result.state === 'denied') {
-            x.innerHTML = "Permission Denied";
-        }
-    })
-    acl = new Accelerometer({frequency: 60});
-    acl.addEventListener('reading', () => {
-        x.innerHTML = "Fucker" + acl.z;
-    })
-    acl.start();*/
 }
 
 function startCount() {
@@ -54,6 +32,6 @@ function countShakes(az) {
         counter++;
     }
     oldAz = az;
-    x.innerHTML = "Z = " + az + "<br>Count = " + counter;
+    x.innerHTML = counter.toString();
 }
 
