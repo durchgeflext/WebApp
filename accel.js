759eb3b149d39d1e2feff851e20534a59a195912
Felix <felix.wedler@t-online.de>
2022-01-25 22:56:22 +0100

    No idea why the last commit didn't work

--- accel.js
+++ accel.js
@@ -32,6 +32,6 @@ function countShakes(az) {
         counter++;
     }
     oldAz = az;
-    x.innerHTML = counter.toString() + "<br>Z = " + az;
+    x.innerHTML = "" + counter + "<br>Z = " + az;
 }
 
